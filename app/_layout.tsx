9bb9a051c9367c238eccef1ec577ee8c2676615a
Subx1s0o <nazarsabardin@gmail.com>
2024-12-06 20:30:47 +0200

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import "../global.css";
+import { SplashScreen, Stack } from "expo-router";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="profile" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
