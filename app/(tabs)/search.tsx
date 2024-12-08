e1ade379b60eda2eb5c0593188e839dd724f6df9
Subx1s0o <nazarsabardin@gmail.com>
2024-12-08 05:26:46 +0200

    feat: add seacrh

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const search = () => {
   return (
-    <View className="bg-primary h-full">
-      <Text className="text-white">search</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <View className="bg-primary h-full px-4 pt-10">
+        <TextInput
+          placeholder="Search..."
+          className="text-white placeholder:text-slate-400 font-poppins text-xl px-10 border border-cherry rounded-3xl h-20"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
